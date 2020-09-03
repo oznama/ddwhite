27924969890c138231642ddfdb3dbf30b65db63c
Oziel Naranjo Marquez <oznama27@gmail.com>
2020-09-03 15:04:14 -0500

    Creating Expense find only current month

--- workspace/ddwhite-ws/src/main/java/mx/com/ddwhite/ws/controller/ExpenseController.java
+++ workspace/ddwhite-ws/src/main/java/mx/com/ddwhite/ws/controller/ExpenseController.java
@@ -1,5 +1,7 @@
 package mx.com.ddwhite.ws.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
@@ -25,7 +27,10 @@ public class ExpenseController implements GenericController<Expense> {
 
 	@Override
 	public Page<Expense> findAll(Pageable pageable) {
-		return repository.findAll(pageable);
+		LocalDateTime now = LocalDateTime.now();
+		String startDate = now.withDayOfMonth(1).toString();
+		String endDate = now.withDayOfMonth(now.getDayOfMonth()).toString();
+		return repository.findOnlyCurrentMonth(startDate, endDate, pageable);
 	}
 
 	@Override
--- workspace/ddwhite-ws/src/main/java/mx/com/ddwhite/ws/repository/ExpenseRepository.java
+++ workspace/ddwhite-ws/src/main/java/mx/com/ddwhite/ws/repository/ExpenseRepository.java
@@ -2,7 +2,11 @@ package mx.com.ddwhite.ws.repository;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import mx.com.ddwhite.ws.model.Expense;
@@ -10,5 +14,8 @@ import mx.com.ddwhite.ws.model.Expense;
 @Repository
 @Transactional
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+	
+	@Query("SELECT e FROM Expense e WHERE e.dateCreated BETWEEN :startDate AND :endDate")
+	Page<Expense> findOnlyCurrentMonth(@Param("startDate") String startDate, @Param("endDate") String endDate, Pageable pageable);
 
 }
